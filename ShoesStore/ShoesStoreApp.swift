6464fc99c4fb979f5b1b18226b128b8edd3df6d0
QuangHaryanto <apple.haryanto@gmail.com>
2023-10-03 16:29:16 +0700

    Initial Commit

---
+++ ShoesStore/ShoesStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoesStoreApp.swift
+//  ShoesStore
+//
+//  Created by Haryanto Salim on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoesStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
